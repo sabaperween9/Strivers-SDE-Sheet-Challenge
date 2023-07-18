8145e696bea807631c7bc13b343742e21757ebfd
saba perween <Tubasaba9@gmail.com>
2023-07-18 14:34:07 +0530

    Allocate Books

---
+++ Day11-BinarySearch/Q7.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public static long ayushGivesNinjatest(int days, int chap, int[] time) {
+        
+        long high = 0;
+        long low = 0;
+        for(int x : time) {
+            high += x;
+        }
+        long res = 0;
+        while(low <= high) {
+            long mid = low + (high - low) / 2;
+            if(isValid(mid, days, time)) {
+                res = mid;
+                high = mid - 1;
+            }else low = mid + 1;
+        }
+        return res;
+    }
+
+    static boolean isValid(long limit, int days, int[] time) {
+        int dayCount = 1;
+        long chapter = 0;
+
+        for(int i = 0; i < time.length; i++) {
+            if(time[i] > limit) return false;
+            if(chapter + time[i] > limit) {
+                dayCount++;
+                chapter = time[i];
+                if(dayCount > days) return false;
+            }else chapter += time[i];
+        }
+
+        return dayCount <= days; 
+    }
+}
